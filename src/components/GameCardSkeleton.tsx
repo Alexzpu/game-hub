74a3f2a8ed70fdf197547bb4616837efde7e77b8
Alex Voronin <lyosha.voronin2703@gmil.com>
2023-06-30 18:09:01 +0400

    fetch the genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSceleton = () => {
   return (
-    <Card >
+    <Card>
       <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
