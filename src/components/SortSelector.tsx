72e83c446325e5590ab109eab65ee0c966a9f279
Alex Voronin <lyosha.voronin2703@gmil.com>
2023-07-05 12:01:27 +0400

    sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,23 +1,62 @@
 import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
 import { BsChevronDoubleDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    {
+      value: "",
+      label: "Relevance",
+    },
+    {
+      value: "-added",
+      label: "Date added",
+    },
+    {
+      value: "name",
+      label: "Name",
+    },
+    {
+      value: "-released",
+      label: "Release date",
+    },
+    {
+      value: "-metacritic",
+      label: "Popularity",
+    },
+    {
+      value: "-rating",
+      label: "Average rating",
+    },
+    {
+      value: "",
+      label: "",
+    },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDoubleDown />}>
-        order by: relevance
+        order by: {currentSortOrder?.label || "Relavence"}
       </MenuButton>
       <MenuList>
-        <MenuItem>zfsdsf</MenuItem>
-        <MenuItem>sdfcasf</MenuItem>
-        <MenuItem>dbsdfv</MenuItem>
-        <MenuItem>xbcvbc</MenuItem>
-        <MenuItem>cvxbxvc</MenuItem>
-        <MenuItem>asdf</MenuItem>
-        <MenuItem>hafdgv</MenuItem>
-        <MenuItem>svnbj,v</MenuItem>
-        <MenuItem>asv</MenuItem>
-        <MenuItem>cvbsfg</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
